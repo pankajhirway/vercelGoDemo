fef85f052af427633f2ff5f1384ba2005501643e
Pankaj H <pankaj.hirway@gmail.com>
2020-07-13 18:54:54 +0530

    fix: Test

--- api/index.go
+++ api/index.go
@@ -1,10 +1,10 @@
 package handler
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "<h1>Hello Pankaj from Go!</h1>")
 }
